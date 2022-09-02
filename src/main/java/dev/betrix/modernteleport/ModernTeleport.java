cc101a2d090e8f06679616dd49d9b8a566c4ba7c
Ryan <betrix303@gmail.com>
2022-09-02 01:27:12 -0400

    add reload subcommand

--- src/main/java/dev/betrix/modernteleport/ModernTeleport.java
+++ src/main/java/dev/betrix/modernteleport/ModernTeleport.java
@@ -1,15 +1,18 @@
 package dev.betrix.modernteleport;
 
+import dev.betrix.modernteleport.commands.ModernTeleportCommand;
 import dev.betrix.modernteleport.commands.TeleportAcceptCommand;
 import dev.betrix.modernteleport.commands.TeleportRejectCommand;
 import dev.betrix.modernteleport.commands.TeleportRequestCommand;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public final class ModernTeleport extends JavaPlugin {
-    
+
     public boolean canUseCommand(Player player) {
         boolean usePermission = getConfig().getBoolean("use_permissions");
 
@@ -20,8 +23,28 @@ public final class ModernTeleport extends JavaPlugin {
         return player.hasPermission("modernteleport.teleport");
     }
 
+    public void playSound(Player player, Key key) {
+        Sound sound = Sound.sound(key, Sound.Source.BLOCK, 1f, 1f);
+
+        player.playSound(sound);
+    }
+
     public String getPrefix() {
         return getConfig().getString("prefix");
+    }
+
+    public void initCommands() {
+        TeleportHandler teleportHandler = new TeleportHandler(this);
+
+
+        Objects.requireNonNull(getCommand("tprequest")).setExecutor(new TeleportRequestCommand(this, teleportHandler));
+        Objects.requireNonNull(getCommand("tpaccept")).setExecutor(new TeleportAcceptCommand(this, teleportHandler));
+        Objects.requireNonNull(getCommand("tpreject")).setExecutor(new TeleportRejectCommand(this, teleportHandler));
+
+        ModernTeleportCommand modernTeleportCommand = new ModernTeleportCommand(this);
+        Objects.requireNonNull(getCommand("modernteleport")).setExecutor(modernTeleportCommand);
+        Objects.requireNonNull(getCommand("modernteleport")).setTabCompleter(modernTeleportCommand);
+
     }
 
     @Override
@@ -29,11 +52,7 @@ public final class ModernTeleport extends JavaPlugin {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
-        TeleportHandler teleportHandler = new TeleportHandler(this);
-
-        Objects.requireNonNull(getCommand("tprequest")).setExecutor(new TeleportRequestCommand(this, teleportHandler));
-        Objects.requireNonNull(getCommand("tpaccept")).setExecutor(new TeleportAcceptCommand(this, teleportHandler));
-        Objects.requireNonNull(getCommand("tpreject")).setExecutor(new TeleportRejectCommand(this, teleportHandler));
+        initCommands();
 
         getLogger().info("Modern Teleport initialized");
     }
---
+++ src/main/java/dev/betrix/modernteleport/commands/ModernTeleportCommand.java
@@ -0,0 +1,58 @@
+package dev.betrix.modernteleport.commands;
+
+import dev.betrix.modernteleport.ModernTeleport;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModernTeleportCommand implements CommandExecutor, TabCompleter {
+
+    private final ModernTeleport modernTeleport;
+    private final Configuration config;
+    private final String prefix;
+
+    public ModernTeleportCommand(ModernTeleport modernTeleport) {
+        this.modernTeleport = modernTeleport;
+        this.config = modernTeleport.getConfig();
+        this.prefix = modernTeleport.getPrefix();
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (sender instanceof Player player) {
+            if (args[0].equals("reload")) {
+                if (player.hasPermission("modernteleport.reload")) {
+                    modernTeleport.reloadConfig();
+
+                    modernTeleport.initCommands();
+
+                    player.sendMessage(
+                            MiniMessage.miniMessage().deserialize(
+                                    config.getString("messages.config_reloaded").replace("%prefix%", prefix)));
+                    modernTeleport.playSound(player, Key.key("entity.experience_orb.pickup"));
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("reload");
+
+        return list;
+    }
+}
